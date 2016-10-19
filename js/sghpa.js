392a4deb9dce925102a61e0c533e6428550fcfa9
tinchodias <tinchodias@gmail.com>
2016-10-19 01:53:25 -0300

    fix

--- js/sghpa.js
+++ js/sghpa.js
@@ -1,8 +1,5 @@
-// Inspired in https://github.com/csuwildcat/sghpa/blob/master/index.html
-(function(){
-  var redirect = sessionStorage.redirect;
-  delete sessionStorage.redirect;
-  if (redirect && redirect != location.href) {
-    history.replaceState(null, null, redirect);
-  }
-})();
+var redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect != location.href) {
+  history.replaceState(null, null, redirect);
+}
